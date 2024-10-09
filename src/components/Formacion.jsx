f8f59fbe939638b6d084699bd31d9c571d844f03
ivandev <ivangtx19@gmail.com>
2024-10-09 13:02:37 -0600

    fix(formacion): Arregla diseños

--- src/components/Formacion.jsx
+++ src/components/Formacion.jsx
@@ -4,7 +4,7 @@ import { FaCode, FaLaptopCode, FaCertificate } from 'react-icons/fa';
 const SectionContainer = styled.section`
   padding: 60px 20px;
   background-color: #f7f9fb; /* Fondo claro para contraste */
-  text-align: center;
+ 
 `;
 
 const SectionTitle = styled.h2`
@@ -41,18 +41,28 @@ const Institution = styled.span`
   font-style: italic;
 `;
 
+const SectionInfo = styled.section`
+ text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  
+`
+
 const Formacion = () => {
   return (
     <SectionContainer id="formacion">
-      <SectionTitle>Formación</SectionTitle>
-      <EducationList>
-        <EducationItem>
-          <FaCode /> <Title>Carrera Técnica en Programación</Title> - <Institution>CECYTEM CHIMALHUACAN II</Institution>, Chimalhuacán, Estado de México (2017-2020)
-        </EducationItem>
-        <EducationItem>
-          <FaLaptopCode /> <Title>Desarrollo Front-End</Title> - <Institution>Alura Latam</Institution> - Remoto (Enero 2024 - Julio 2024)
-        </EducationItem>
-      </EducationList>
+      <SectionInfo>
+        <SectionTitle>Formación</SectionTitle>
+        <EducationList>
+          <EducationItem>
+            <FaCode /> <Title>Carrera Técnica en Programación</Title> - <Institution>CECYTEM CHIMALHUACAN II</Institution>, Chimalhuacán, Estado de México (2017-2020)
+          </EducationItem>
+          <EducationItem>
+            <FaLaptopCode /> <Title>Desarrollo Front-End</Title> - <Institution>Alura Latam</Institution> - Remoto (Enero 2024 - Julio 2024)
+          </EducationItem>
+        </EducationList>
+      </SectionInfo>
     </SectionContainer>
   );
 };
